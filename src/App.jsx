bfb71f32363f244009f8de39cddca2a2cdbce5c4
Guilherme da Silva <guilhermesilva.vida@gmail.com>
2025-11-26 17:56:13 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from "./componentes/props/Login.jsx"   
+
+function App() {                                   
+
+  return (                                         
+    <>                                             
+     <Login />                                     
+    </>                                            
+  )
+}
+
+export default App                                 
---
+++ src/componentes/props/Labelln.jsx
@@ -0,0 +1,11 @@
+import React from 'react';                      
+
+export default function Labelln({ texto }) {    
+                                                
+
+  return (                                      
+    <>                                          
+      {texto} <br />                            
+    </>                                         
+  );                                            
+}                                               
